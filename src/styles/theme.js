fcf8d4afa8638f7344a40d1ca24741725016a27d
Joan Gavelán <joangavelan@gmail.com>
2021-11-17 18:53:29 -0500

    theme fonts and colors setup

---
+++ src/styles/theme.js
@@ -0,0 +1,20 @@
+const theme = {
+  colors: {
+    blue: {
+      100: '#387AFF',
+      200: '#2362E1'
+    },
+    dark: {
+      100: '#181A1B',
+      200: '#161819',
+      300: '#454545',
+      400: '#27292E'
+    },
+    white: '#CECECF'
+  },
+  font: {
+    primary: 'Roboto, Helvetica, Arial, sans-serif'
+  }
+}
+
+export default theme
