6f6a1f8f128703c40fe5c2f7873b4c3eeec222f2
Joan Gavelán <joangavelan@gmail.com>
2021-11-17 19:26:50 -0500

    initial App component setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from 'react'
+import styled from 'styled-components'
+import GlobalStyles from './styles/GlobalStyles'
+import theme from './styles/theme'
+const { colors } = theme;
+
+const Container = styled.div`
+  max-width: 100rem;
+  margin: 0 auto;
+  padding: 3rem;
+  min-height: 100vh;
+  background-color: palegoldenrod;
+`
 
 const App = () => {
   return (
-    <div>
+    <Container>
+      <GlobalStyles />
       
-    </div>
+      
+
+    </Container>
   )
 }
 
